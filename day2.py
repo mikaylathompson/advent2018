356c8ef3ef377deea70f1b57a5a9dfbe7dddccff
Mikayla Thompson <mikaylathompson@bainbridgehealth.com>
2018-12-06 10:20:34 -0500

    Add days 2 and 3

---
+++ day2.py
@@ -0,0 +1,32 @@
+from collections import Counter
+
+def fn1(inpt_lines):
+    twos = 0
+    threes = 0
+    for line in inpt_lines:
+        cnt = Counter(line)
+        if 2 in cnt.values():
+            twos += 1
+        if 3 in cnt.values():
+            threes += 1
+    return twos * threes
+
+def fn2(inpt_lines):
+    # Add entries to a dictionary in form:
+        # {"*bcde": ["abcde", "xbcde", "yxcde"], "a*cde": ["abcde"], "ab*de": ["abcde"] ...}
+    # Actually, I can do it as a set, and if I ever find a match, that's the correct one.
+    prototypes = set()
+    for word in inpt_lines:
+        word = word.strip()
+        for i in range(len(word) - 1):
+            new_word = "{0}*{1}".format(word[:i], word[i+1:])
+            if new_word in prototypes:
+                return new_word.replace('*', '')
+            prototypes.add(new_word)
+    return None
+
+if __name__ == '__main__':
+    with open('day2.txt', 'r') as inpt:
+        print(fn1(inpt.readlines()))
+        inpt.seek(0)
+        print(fn2(inpt.readlines()))
---
+++ day3.py
@@ -0,0 +1,47 @@
+import re
+
+clm_re = re.compile(r"#(?P<id>\d+) @ (?P<left>\d+),(?P<top>\d+): (?P<width>\d+)x(?P<height>\d+)")
+
+def fn1(inpt_lines):
+    # Once-seen and twice-seen sets. Once a square is seen, it's added to the once-seen set as "{left}-{right}".
+    # If it's seen again, it's added to the twice-seen set.
+    # 96477 is TOO LOW
+    once_seen = set()
+    twice_seen = set()
+    for line in inpt_lines:
+        claim = clm_re.match(line.strip())
+        left, top, width, height = map(int, claim.group('left', 'top', 'width', 'height'))
+        for x in range(left, left+width):
+            for y in range(top, top+height):
+                pos = "{x}-{y}".format(x=x, y=y)
+                if pos in once_seen:
+                    twice_seen.add(pos)
+                once_seen.add(pos)
+    return len(twice_seen)
+
+def fn2(inpt_lines):
+    once_seen = set()
+    twice_seen = set()
+    claims = {}
+    for line in inpt_lines:
+        claim = clm_re.match(line.strip())
+        left, top, width, height = map(int, claim.group('left', 'top', 'width', 'height'))
+        claim_set = set()
+        for x in range(left, left+width):
+            for y in range(top, top+height):
+                pos = "{x}-{y}".format(x=x, y=y)
+                claim_set.add(pos)
+                if pos in once_seen:
+                    twice_seen.add(pos)
+                once_seen.add(pos)
+        claims[claim.group('id')] = claim_set
+    for claim in claims.keys():
+        if claims[claim].isdisjoint(twice_seen):
+            return claim
+    return None
+
+if __name__ == '__main__':
+    with open('day3.txt', 'r') as inpt:
+        print(fn1(inpt.readlines()))
+        inpt.seek(0)
+        print(fn2(inpt.readlines()))
