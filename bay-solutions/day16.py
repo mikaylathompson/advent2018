4f05b31af061610bbed58c3e2bf604037c92b40d
Bay Gross <bgross17@gmail.com>
2018-12-08 15:03:40 -0500

    creating stub files
    creating stub files

---
+++ bay-solutions/day16.py
@@ -0,0 +1,20 @@
+import re
+import string
+
+def printArr(x):
+  for i in x:
+    print(i)
+
+def solution1(inpt_lines): 
+  return 0
+
+def solution2(inpt_lines):
+  return 0
+
+## Load input data and run our two main functions
+if __name__ == '__main__':
+  with open('../puzzle-input/day16_2018.txt', 'r') as file:
+    inpt_lines = file.read().splitlines()
+
+  print( solution1(inpt_lines) )
+  print( solution2(inpt_lines) )
